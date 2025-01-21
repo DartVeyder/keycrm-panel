6545e71db5f00fd67f74611f876a2fe2dfd94e40
DartVeyder <medvsd.dmitro@gmail.com>
2025-01-21 12:03:52 +0200

    done change status order prestashop

--- webhook_change_order_status.php
+++ webhook_change_order_status.php
@@ -17,27 +17,25 @@ $statusPS = [
 ];
 $orderKC = $keyCrm->webhookOrder();
 
-if($orderKC){
+
 
 $global_source_uuid = explode('-', $orderKC['global_source_uuid']);
 
-$orderKC_id =  $orderKC['id'];
-$orderKC_source_id = $orderKC['source_id'] ;
-$idOrder = $global_source_uuid[1];
-$groupStatusId = $orderKC['status_group_id'];
+//$orderKC_id =  $orderKC['id'];
+//$orderKC_source_id = $orderKC['source_id'] ;
+//$idOrder = $global_source_uuid[1];
+//$groupStatusId = $orderKC['status_group_id'];
 
-//$orderKC_id =  100000;
-//$orderKC_source_id = 18 ;
-//$idOrder = 8002;
-//$groupStatusId = 2;
+$orderKC_id =  100000;
+$orderKC_source_id = 18 ;
+$idOrder = 8392;
+$groupStatusId = 2;
 
 $idOrderState = $statusPS[$groupStatusId];
 
-
-
 if( $orderKC_source_id == 18) {
-    $orderPS = $prestashop->getOrder((int)$idOrder);
-    $text = date("Y-m-d H:i:s") . " orderKC_id: " . $orderKC_id . " orderPS_id: " . $idOrder . " status_group_id: " . $groupStatusId . " source_id: " . $orderKC_source_id . ' current_state_PS: ' . $orderPS['current_state'];
+    $orderPS = $prestashop->getOrder($idOrder);
+    $text = date("Y-m-d H:i:s") . " orderKC_id: " . $orderKC_id . " orderPS_id: " . $idOrder . " status_group_id: " . $groupStatusId . " source_id: " . $orderKC_source_id . ' current_state_PS: ' . $orderPS['current_state'] . json_encode($orderPS) ;
     echo $text;
     $file = fopen('logs/changeOrderStatus.txt', 'a+');
     fwrite($file, $text . "\n");
@@ -46,6 +44,4 @@ if( $orderKC_source_id == 18) {
     if ($orderPS['current_state'] != $idOrderState) {
         $prestashop->changeOrderStatus($idOrder, $idOrderState);
     }
-}
-
 }
